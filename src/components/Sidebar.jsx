efca755bf9770fe0788fe7109cc03e994d94091d
Daniel Meshack Wako <DANIEL.WAKO98@GMAIL.COM>
2024-07-19 19:49:07 +0300

    Modify of next-auth sign in and sign out

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,13 @@
+"use client"
+
 import { FaXTwitter } from "react-icons/fa6";
-import { BiSolidHome } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 
 export default function Sidebar() {
+    const { data: session } = useSession();
     return (
         <div className="flex flex-col gap-5 p-6">
             <Link href="/">
@@ -20,14 +23,25 @@ export default function Sidebar() {
                 <span className="font-bold hidden xl:inline">
                     Home
                 </span>
+
             </Link>
-            <button className="bg-blue-500 text-white
-            font-bold rounded-full px-4 py-2 mt-4 hover:bg-blue-600
-            transition-all duration-200 w-48 h-9 hidden xl:inline">
-                Sign In
-            </button>
-
-
+            {session ? (
+                <button className="bg-blue-500 text-white
+  font-bold rounded-full px-4 py-2 mt-4 hover:bg-blue-600
+  transition-all duration-200 w-48 h-9 hidden xl:inline"
+                    onClick={() => signOut()}
+                >
+                    Sign Out
+                </button>
+            ) : (
+                <button className="bg-blue-500 text-white
+                                     font-bold rounded-full px-4 py-2 mt-4 hover:bg-cyan-600
+                                     transition-all duration-200 w-48 h-9 hidden xl:inline"
+                    onClick={() => signIn()}
+                >
+                    Sign In
+                </button>
+            )};
         </div>
-    )
+    );
 }
