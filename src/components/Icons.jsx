c5e1c6de625f5090e425487910b0b0363de8da03
Daniel Meshack Wako <DANIEL.WAKO98@GMAIL.COM>
2024-07-22 16:18:26 +0300

    Update icons components to handle likes

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,17 +1,73 @@
-"use client"
+'use client';
 
-import { HiOutlineChat, HiOutlineHeart, HiOutlineTrash } from "react-icons/hi"
+import {
+    collection,
+    deleteDoc,
+    doc,
+    getFirestore,
+    onSnapshot,
+    setDoc,
+} from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { HiOutlineHeart, HiHeart } from 'react-icons/hi';
+import { app } from '../firebase';
 
-export default function Icons() {
+export default function LikeSection({ id }) {
+    const { data: session } = useSession();
+    const [hasLiked, setHasLiked] = useState(false);
+    const [likes, setLikes] = useState([]);
+    const db = getFirestore(app);
+
+    useEffect(() => {
+        onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) => {
+            setLikes(snapshot.docs);
+        });
+    }, [db]);
+
+    useEffect(() => {
+        if (likes.findIndex((like) => like.id === session?.user?.uid) !== -1) {
+            setHasLiked(true);
+        } else {
+            setHasLiked(false);
+        }
+    }, [likes]);
+
+    async function likePost() {
+        console.log(db, id, 'likes', session?.user?.uid);
+        if (hasLiked) {
+            await deleteDoc(doc(db, 'posts', id, 'likes', session?.user?.uid));
+        } else {
+            await setDoc(doc(db, 'posts', id, 'likes', session?.user?.uid), {
+                username: session?.user?.username,
+            });
+        }
+    }
+
     return (
-        <div className="flex justify-between  p-2 text-gray-300">
-            <HiOutlineChat
-                className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-outp-2 hover:text-sky-500 hover:bg-sky-100" />
-            <HiOutlineHeart
-                className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-outp-2 hover:text-red-500 hover:bg-red-100" />
-            <HiOutlineTrash
-                className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-outp-2 hover:text-red-500 hover:bg-red-100" />
-
+        <div>
+            {session && (
+                <div className='flex border-t border-gray-100 px-4 pt-4'>
+                    <div className='flex items-center gap-2'>
+                        {hasLiked ? (
+                            <HiHeart
+                                onClick={likePost}
+                                className='text-red-500 cursor-pointer text-3xl  hover:scale-125 transition-transform duration-200 ease-out'
+                            />
+                        ) : (
+                            <HiOutlineHeart
+                                onClick={likePost}
+                                className='cursor-pointer text-3xl  hover:scale-125 transition-transform duration-200 ease-out'
+                            />
+                        )}
+                        {likes.length > 0 && (
+                            <p className='text-gray-500'>
+                                {likes.length} {likes.length === 1 ? 'like' : 'likes'}
+                            </p>
+                        )}
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
