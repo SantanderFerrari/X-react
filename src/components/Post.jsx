6e870b473a755da19756ff87cc04ecd691c201b9
Daniel Meshack Wako <DANIEL.WAKO98@GMAIL.COM>
2024-07-22 15:20:51 +0300

    add icons components to post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { HiDotsHorizontal } from "react-icons/hi"
+import Icons from "./Icons"
 
 export default function Post({ post, id }) {
     return (
@@ -24,6 +25,7 @@ export default function Post({ post, id }) {
                 <Link href={`/posts/${id}`}>
                     <img src={post?.image} className="rounded-2xl mr-2 " ></img>
                 </Link>
+                <Icons />
             </div>
         </div>
     )
